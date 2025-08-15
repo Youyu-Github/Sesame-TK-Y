243252a9afa52bcd325d53ea672d6761740ee22d
Youyu-Github <77781701+Youyu-Github@users.noreply.github.com>
2025-08-15 22:16:58 +0800

    .

--- app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.kt
@@ -12,7 +12,7 @@ import fansirsqi.xposed.sesame.task.antSports.AntSports
 import fansirsqi.xposed.sesame.task.antStall.AntStall
 import fansirsqi.xposed.sesame.task.reserve.Reserve
 
-import fansirsqi.xposed.sesame.task.ancientTree.AncientTree
+import fansirsqi.xposed.sesame.task.ancientTree.EcologicalProtection
 import fansirsqi.xposed.sesame.task.greenFinance.GreenFinance
 import fansirsqi.xposed.sesame.task.consumeGold.ConsumeGold
 //import fansirsqi.xposed.sesame.task.omegakoiTown.OmegakoiTown
@@ -25,7 +25,7 @@ object ModelOrder {
         AntOrchard::class.java,    // 农场
         AntOcean::class.java,        // 海洋
         AntDodo::class.java,       // 神奇物种
-        AncientTree::class.java,     // 古树
+        EcologicalProtection::class.java,     // 古树
         AntCooperate::class.java,    // 合种
         Reserve::class.java,       // 保护地
         AntSports::class.java,       // 运动
