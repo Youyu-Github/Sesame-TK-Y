071377e4159fedeb8074c518cabdd1f30dcf2cdb
Youyu-Github <77781701+Youyu-Github@users.noreply.github.com>
2025-08-19 22:31:50 +0800

    删除小镇

--- app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.kt
@@ -15,7 +15,6 @@ import fansirsqi.xposed.sesame.task.reserve.Reserve
 import fansirsqi.xposed.sesame.task.ancientTree.EcologicalProtection
 import fansirsqi.xposed.sesame.task.greenFinance.GreenFinance
 import fansirsqi.xposed.sesame.task.consumeGold.ConsumeGold
-//import fansirsqi.xposed.sesame.task.omegakoiTown.OmegakoiTown
 
 object ModelOrder {
     private val array = arrayOf(
@@ -34,7 +33,6 @@ object ModelOrder {
         GreenFinance::class.java,  // 绿色经营
 //        Antinvoice::class.java,      // 蚂蚁发票
         ConsumeGold::class.java,   // 消费金
-//        OmegakoiTown::class.java,  // 小镇
         AnswerAI::class.java         // AI答题
     )
 
