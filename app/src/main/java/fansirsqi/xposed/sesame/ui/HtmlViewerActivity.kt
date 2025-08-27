67cf384125fe56d9b280438058dcf9f24492938d
Youyu-Github <77781701+Youyu-Github@users.noreply.github.com>
2025-08-27 21:07:13 +0800

    Update HtmlViewerActivity.kt

--- app/src/main/java/fansirsqi/xposed/sesame/ui/HtmlViewerActivity.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/ui/HtmlViewerActivity.kt
@@ -185,7 +185,7 @@ class HtmlViewerActivity : BaseActivity() {
 
                     webView.loadUrl("file:///android_asset/log_viewer.html")
                     // webView.webChromeClient = object : WebChromeClient() {
-                    webView.setwebChromeClient = object : WebChromeClient() {
+                    webView.WebChromeClient = object : WebChromeClient() {
                         override fun onProgressChanged(view: WebView?, progress: Int) {
                             progressBar?.progress = progress
                             if (progress < 100) {
@@ -315,8 +315,6 @@ class HtmlViewerActivity : BaseActivity() {
             try {
                 mWebView?.loadUrl("about:blank")
                 mWebView?.stopLoading()
-                mWebView?.setwebChromeClient = null
-                mWebView?.setWebViewClient = null
                 mWebView?.destroy()
             } catch (ignore: Throwable) {
                 // 异常处理
