2b5271f3b188a802ba696778ab70a39852c6ea37
Youyu-Github <77781701+Youyu-Github@users.noreply.github.com>
2025-10-18 22:16:22 +0800

    🍃 feat(alipay):调整重启唤醒参数，不再直接拉起前台避免影响日用其他体验

--- app/src/main/java/fansirsqi/xposed/sesame/data/General.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/data/General.kt
@@ -20,6 +20,9 @@ object General {
      */
     const val CURRENT_USING_ACTIVITY: String = "com.eg.android.AlipayGphone.AlipayLogin"
 
+    const val CURRENT_USING_SCHEME_ACTIVITY = "com.alipay.mobile.framework.service.common.SchemeStartActivity"
+
+
     /**
      * JSON对象的类名。
      */
