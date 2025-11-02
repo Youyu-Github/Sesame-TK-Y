ee8cd0261bfb184114177a417c46945421adf52e
Youyu-Github <77781701+Youyu-Github@users.noreply.github.com>
2025-11-02 14:09:12 +0800

    添加屏蔽日志

--- app/src/main/java/fansirsqi/xposed/sesame/util/ResChecker.java
+++ app/src/main/java/fansirsqi/xposed/sesame/util/ResChecker.java
@@ -50,6 +50,12 @@ public class ResChecker {
                     memo.contains("我的减肥计划完蛋了") ||
                     memo.contains("当日达到上限") ||
                     memo.contains("有人抢在你前面") ||
+                    memo.contains("同一好友新村，只能摆一个小摊哦") ||
+                    memo.contains("今日助力次数已用完") ||
+                    memo.contains("TA的小摊已被请走") ||
+                    memo.contains("收摊成功，快回我的新村看看吧") ||
+                    memo.contains("不支持rpc完成的任务") ||
+                    memo.contains("饲料槽已满") ||
                     memo.contains("手速太快")) {
                 return false; // 返回false，但不打印错误日志
             }
